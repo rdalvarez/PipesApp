7cb40273d96848330c243c6966cefffe2ee6867d
rdalvarez <david_de_odin@hotmail.com>
2021-10-23 17:50:35 -0300

    PrimeNg - MenuBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+// Modulo Personalizado
 import { SharedModule } from './shared/shared.module';
+
 
 @NgModule({
   declarations: [
