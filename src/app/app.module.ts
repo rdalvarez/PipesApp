1f554ff9e6ae99efa112a1998fdab1b91019d6dc
rdalvarez <david_de_odin@hotmail.com>
2021-10-26 21:36:19 -0300

    PrimeFlex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 // Modulo Personalizado
 import { SharedModule } from './shared/shared.module';
 import { AppRouterModule } from './app-router.module';
+import { VentasModule } from './ventas/ventas.module';
 
 
 @NgModule({
@@ -16,6 +17,7 @@ import { AppRouterModule } from './app-router.module';
     BrowserModule,
     AppRouterModule,
     SharedModule,
+    VentasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
