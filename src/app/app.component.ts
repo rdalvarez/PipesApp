e22d175fdb6a8c887f599afbacae32f3b190f375
rdalvarez <david_de_odin@hotmail.com>
2021-11-09 19:12:15 -0300

    PrimeNg - Fieldset

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from "primeng/api";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   
-  nombre: string = 'daVid alvArez';
-  valor: number = 1000;
-  obj = {
-    nombre: 'David'
+  constructor(private primeNGConfig: PrimeNGConfig){ }
+
+  ngOnInit(): void {
+      this.primeNGConfig.ripple = true;    
   }
-  mostrarNombre() {
-    console.log( this.nombre );
-    console.log( this.valor );
-    console.log( this.obj );
-  }
+  
 }
